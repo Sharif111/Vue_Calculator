e9e4b8dc9d4593e7437133eb10723e382e1a6cc8
sharif111 <sijanewubd@gmail.com>
2021-11-30 17:47:27 +0600

    initial commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="calculator">
+    <div class="display">0.0521</div>
+    <div class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn">+</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn">-</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn">*</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn">/</div>
+    <div class="btn">0</div>
+    <div class="btn">.</div>
+    <div class="btn">=</div>
+    <div class="btn">AC</div>
+    
+    </div>
+
+
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+
+<style scoped>
+.calculator {
+  margin: 0 auto;
+  width: 400px;
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+
+.display {
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
+}
+
+.zero {
+  grid-column: 1 / 3;
+}
+
+.btn {
+  background-color: #F2F2F2;
+  border: 1px solid #999;
+}
+
+.operator {
+  background-color: orange;
+  color: white;
+}
+</style>
